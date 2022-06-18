15372f704d925a7bb702523f11d7efc60539516f
Jess M <eveyj777@gmail.com>
2022-06-18 18:45:37 +0100

    swap tweet and new quote button places

--- quote-generator/src/QuoteBox.js
+++ quote-generator/src/QuoteBox.js
@@ -46,8 +46,8 @@ class QuoteBox extends React.Component {
                     <figcaption className="blockquote-footer" id="author"><cite title={this.state.author}>{this.state.author}</cite></figcaption>
                 </figure>
                 <div className="row">
-                    <div className="col"><button id="new-quote" type="button" className="btn btn-primary" onClick={this.handleClick}>New Quote</button></div>
                     <div className="col"><a className="button" id="tweet-quote" target="_blank" href="twitter.com/intent/tweet"><i className="bi bi-twitter"></i></a></div>
+                    <div className="col"><button id="new-quote" type="button" className="btn btn-primary" onClick={this.handleClick}>New Quote</button></div>
                 </div>
             </div >
         );
